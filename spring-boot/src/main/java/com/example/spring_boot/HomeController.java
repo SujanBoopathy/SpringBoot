7a6258b24038cba16339b5cf4ed5df2978482023
SujanBoopathy <sujansaradha@gmail.com>
2024-06-16 20:22:04 +0530

    spring boot

--- spring-boot/src/main/java/com/example/spring_boot/HomeController.java
+++ spring-boot/src/main/java/com/example/spring_boot/HomeController.java
@@ -1,12 +1,13 @@
 package com.example.spring_boot;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/")
     public String index(){
-        return "Greeting from spring boot";
+        return "home";
     }
 }
