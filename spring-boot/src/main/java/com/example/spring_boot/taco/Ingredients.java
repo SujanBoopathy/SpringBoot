86bd3ee263a86228a545ec33f10ded87c2f7c988
SujanBoopathy <sujansaradha@gmail.com>
2024-06-18 21:10:43 +0530

    spring boot

--- spring-boot/src/main/java/com/example/spring_boot/taco/Ingredients.java
+++ spring-boot/src/main/java/com/example/spring_boot/taco/Ingredients.java
@@ -5,6 +5,39 @@ public class Ingredients {
     public String name;
     public String type;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredients{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
---
+++ spring-boot/src/main/java/com/example/spring_boot/taco/Taco.java
@@ -0,0 +1,32 @@
+package com.example.spring_boot.taco;
+
+import java.util.List;
+
+public class Taco {
+    private String name;
+    private List<Ingredients> ingredients;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredients> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return "Taco{" +
+                "name='" + name + '\'' +
+                ", ingredients=" + ingredients +
+                '}';
+    }
+}
