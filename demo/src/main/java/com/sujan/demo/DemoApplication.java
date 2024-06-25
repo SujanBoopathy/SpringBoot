cfb9fe8b996e15a22511e438ddcb0fb0a1bb1c2a
SujanBoopathy <sujansaradha@gmail.com>
2024-06-25 21:52:48 +0530

    spring boot

--- demo/src/main/java/com/sujan/demo/DemoApplication.java
+++ demo/src/main/java/com/sujan/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.sujan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
 
+		HelloWorld helloWorld = applicationContext.getBean(HelloWorld.class);
+		helloWorld.sayHello();
+	}
+//	@Bean
+//	public HelloWorld helloWorld(){
+//		return new HelloWorld();
+//	}
 }
---
+++ demo/src/main/java/com/sujan/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sujan.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+    public void sayHello(){
+        System.out.println("hello world!");
+    }
+}
