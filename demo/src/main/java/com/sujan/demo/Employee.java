23efde6c5d9bb51de9cf74c9ebc846483795729f
SujanBoopathy <sujansaradha@gmail.com>
2024-07-20 22:12:15 +0530

    spring boot

--- demo/src/main/java/com/sujan/demo/Employee.java
+++ demo/src/main/java/com/sujan/demo/Employee.java
@@ -1,5 +1,50 @@
 package com.sujan.demo;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
 
+  @Id
+  private Integer id;
+  
+  private String firstName;
+  
+  private String lastName;
+  
+  private LocalDate dateOfBirth;
+  
+  // Hibernate expects entities to have a no-arg constructor,
+  // though it does not necessarily have to be public.
+  private Employee() {}
+  
+  public Employee(String firstName, String lastName, LocalDate dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
+  
+  public Integer getId() {
+    return this.id;
+  }
+  
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public LocalDate getDateOfBirth() {
+    return this.dateOfBirth;
+  }
 }
