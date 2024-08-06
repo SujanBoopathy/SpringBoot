14c49f9c98607b05d5c72552f24a254f26a24e20
SujanBoopathy <sujansaradha@gmail.com>
2024-08-06 21:35:09 +0530

    spring data

---
+++ spring_data/src/main/java/com/spring/database/spring_data/Course.java
@@ -0,0 +1,57 @@
+package com.spring.database.spring_data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Course {
+	private int courseId;
+	private int credit;
+	private String title;
+	
+	public Course() {
+
+	}
+	public Course(int courseId, int credit, String title) {
+		this.courseId = courseId;
+		this.credit = credit;
+		this.title = title;
+	}
+	
+	@Id
+	@SequenceGenerator(
+			name = "course_sequence",
+			sequenceName = "course_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "course_sequence"
+	)
+	public int getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	public int getCredit() {
+		return credit;
+	}
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@Override
+	public String toString() {
+		return "Course [courseId=" + courseId + ", credit=" + credit + ", title=" + title + "]";
+	}
+}
---
+++ spring_data/src/main/java/com/spring/database/spring_data/CourseMaterial.java
@@ -0,0 +1,64 @@
+package com.spring.database.spring_data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class CourseMaterial {
+	@Id
+	@SequenceGenerator(
+			name = "course_material_sequence",
+			sequenceName = "course_material_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "course_material_sequence"
+	)
+	private int courseMaterialId;
+	private String url;
+	private Course course;
+	
+	public CourseMaterial(int courseMaterialId, String url, Course course) {
+		super();
+		this.courseMaterialId = courseMaterialId;
+		this.url = url;
+		this.course = course;
+	}
+	
+	public CourseMaterial() {
+		super();
+	}
+
+	public int getCourseMaterialId() {
+		return courseMaterialId;
+	}
+
+	public void setCourseMaterialId(int courseMaterialId) {
+		this.courseMaterialId = courseMaterialId;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	@Override
+	public String toString() {
+		return "CourseMaterial [courseMaterialId=" + courseMaterialId + ", url=" + url + ", course=" + course + "]";
+	}
+}
